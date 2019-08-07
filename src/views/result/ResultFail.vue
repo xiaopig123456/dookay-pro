1a0d509273b72a07fcd8c8b488554e6f0a901431
朱言俊 <zhuyanjun@dookay.com>
2019-08-07 17:59:24 +0800

    完善结果页

--- src/views/result/ResultFail.vue
+++ src/views/result/ResultFail.vue
@@ -1,7 +1,11 @@
 <template>
     <el-card class="dk-box-card-main" shadow="never">
         <dk-result class="dk-page-result" state="error" title="提交失败" content="请核对并修改以下信息后，再重新提交。">
-
+            <div class="dk-page-result-content">
+                <div class="dk-page-result-content-title">您提交的内容有如下错误：</div>
+                <p><i class="el-icon-circle-close dk-text-danger dk-mr-2"></i>您的账户已被冻结 <a href="javascript:void(0);" class="dk-ml-4">立即解冻 <i class="el-icon-arrow-right"></i></a></p>
+                <p><i class="el-icon-circle-close dk-text-danger dk-mr-2"></i>您的账户还不具备申请资格 <a href="javascript:void(0);" class="dk-ml-4">立即解冻 <i class="el-icon-arrow-right"></i></a></p>
+            </div>
         </dk-result>
     </el-card>
 </template>
